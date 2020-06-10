39fd7aa76321a4ed9565d18bfa2cd500fb066203
root <root@ip-172-31-29-182.eu-west-3.compute.internal>
2020-06-10 10:00:34 +0000

    add env file + update index.js file

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const http = require('http');
 const port = process.env.PORT || 3000;
+const environment = process.env.ENV;
+const appVersion = process.env.APP_VERSION;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node!\n'
+  const msg = 'Hello World! ' + appVersion + ' - ' + environment;
   res.end(msg);
 });
 
